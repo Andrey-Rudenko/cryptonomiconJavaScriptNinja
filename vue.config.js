dc5eaede680ce4280a76077d836858c442bd8387
Andrey <andrey.rudenko.200333@gmail.com>
2023-11-07 22:26:39 +1000

    испарвление косяка

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  paablicPath: `/cryptonomiconJavaScriptNinja /`,
+  publicPath: `/cryptonomiconJavaScriptNinja /`,
   transpileDependencies: true
 })
